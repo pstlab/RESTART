0e482d7204158f0b3b438262d1d7bc2a5c7d1d73
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2023-12-25 10:36:25 +0100

    Add rationet as a subdirectory and link it to the project

--- include/restart_app.h
+++ include/restart_app.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "coco_core.h"
+#include "server.hpp"
 
 namespace restart
 {
   class restart_db;
 
-  class restart_app : public coco::coco_core
+  class restart_app : public coco::coco_core, public network::server
   {
   public:
     restart_app(restart_db &db);
