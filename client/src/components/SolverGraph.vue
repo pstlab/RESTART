b91cd604b30cdc1fa5fcb50999506099ccb9f50b
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2023-12-28 16:15:23 +0100

    Refactor SolverGraph layout configuration

--- client/src/components/SolverGraph.vue
+++ client/src/components/SolverGraph.vue
@@ -19,12 +19,16 @@ const get_graph_id = (solver) => 'slv-' + solver.id + '-graph';
 
 cytoscape.use(klay);
 let cy;
-let layout;
+let layout = {
+  name: 'klay',
+  fit: false,
+  animate: true
+};
 
 const node_listeners = new Map();
 const new_node_listener = (node) => {
   cy.add({ group: 'nodes', data: { id: node.id, type: 'phi' in node ? 'flaw' : 'resolver', label: node.label, state: node.state, cost: node.cost } });
-  layout.run();
+  cy.layout(layout).run();
   const node_listener = (node) => { cy.$id(node.id).data({ label: node.label, state: node.state, cost: node.cost }); };
   props.solver.add_node_listener(node, node_listener);
   node_listeners.set(node, node_listener);
@@ -33,7 +37,7 @@ const new_node_listener = (node) => {
 const edge_listeners = new Map();
 const new_edge_listener = (edge) => {
   cy.add({ group: 'edges', data: { id: edge.from + '-' + edge.to, source: edge.from, target: edge.to, state: edge.state } });
-  layout.run();
+  cy.layout(layout).run();
   const edge_listener = (edge) => { cy.$id(edge.from + '-' + edge.to).data({ state: edge.state }); };
   props.solver.add_edge_listener(edge, edge_listener);
   edge_listeners.set(edge, edge_listener);
@@ -74,13 +78,7 @@ onMounted(() => {
   props.solver.add_new_node_listener(new_node_listener);
   props.solver.add_new_edge_listener(new_edge_listener);
 
-  layout = cy.layout({
-    name: 'klay',
-    animate: true,
-    klay: {
-      direction: 'RIGHT',
-    },
-  });
+  cy.layout(layout).run();
 });
 
 onUnmounted(() => {
