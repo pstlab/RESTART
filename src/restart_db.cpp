6edb6de71e78d3411e9e099bcb4dbebb97d7de9c
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-09-03 10:51:11 +0200

    Update extern/coco and extern/rationet submodules

--- src/restart_db.cpp
+++
@@ -1,8 +0,0 @@
-#include "restart_db.h"
-
-namespace restart
-{
-  restart_db::restart_db(const std::string &root, const std::string &mongodb_uri) : mongo_db(root, mongodb_uri)
-  {
-  }
-} // namespace restart
