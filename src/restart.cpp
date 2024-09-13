a84e4673f47e5c888707d97e48bdc43afbe0d226
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-09-13 14:36:12 +0200

    Update compose.yaml and restart.cpp

--- src/restart.cpp
+++ src/restart.cpp
@@ -2,8 +2,7 @@
 
 #include "restart_db.hpp"
 
-namespace restart {
-
-  restart::restart(const std::string &host, unsigned short port, const std::string &mongodb_uri) : coco_server(host, port, std::make_unique<restart_db>(mongodb_uri)) {
-  }
+namespace restart
+{
+  restart::restart(const std::string &host, unsigned short port, const std::string &mongodb_uri) : coco_server(host, port, std::make_unique<restart_db>(mongodb_uri)) {}
 } // namespace restart
