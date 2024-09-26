2b5144bcf883e6537f4f424b8a15e284d6efbcd6
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-09-26 17:17:46 +0200

    Update items.py to send data to MyndSync item periodically

--- tests/items.py
+++ tests/items.py
@@ -1,5 +1,6 @@
 import sys
 import requests
+import time
 
 def init_db(url):
     # Get the MyndSync type
@@ -8,9 +9,16 @@ def init_db(url):
     print(myndsync_type)
 
     # Create the MyndSync item
-    response = requests.post(url + '/item', json={'type': myndsync_type['id'], 'properties': {'name': 'MyndSync1'}})
+    response = requests.post(url + '/item', json={'type': myndsync_type['id'], 'properties': {'name': 'MyndSync42'}})
     myndsync_item = response.json()
     print(myndsync_item)
+    print('MyndSync item ID:', myndsync_item['id'])
+
+    # Send data to the MyndSync item
+    while True:
+        time.sleep(10)
+        response = requests.post(url + '/data/' + myndsync_item['id'], json={'nickname': '42', 'average_response_time': 5, 'first_click_time': 3, 'helps_nr': 0, 'total_lives_nr': 3, 'retries_nr': 0, 'game_time': 'Sono le ore 12:00'})
+        print(response.status_code)
 
 if __name__ == '__main__':
     url = sys.argv[1] if len(sys.argv) > 1 else 'http://localhost:8080'
